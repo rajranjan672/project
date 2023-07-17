91991ba456c182a398dbabfdcd0f79babac1b769
Ujala singh <100126092+ujalasingh66@users.noreply.github.com>
2023-07-17 12:23:57 +0530

    check now

--- src/Components/Ideas.js
+++ src/Components/Ideas.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import "../Components/Ideas.css"
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -6,10 +7,13 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
+
  function Ideas() {
   return (
     <>
-    <Card sx={{ maxWidth: 345 }}>
+    <div className='idea me-1'>
+      <div style={{justifyContent:'space-between'}}>
+    <Card sx={{ maxWidth: 310 ,variant:"outlined" }} >
       <CardMedia
         sx={{ height: 140 }}
         image="/static/images/cards/contemplative-reptile.jpg"
@@ -25,10 +29,80 @@ import Typography from '@mui/material/Typography';
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <Button size="small">Edit</Button>
+        <Button size="small">Delete</Button>
       </CardActions>
     </Card>
+    </div>
+    <br/><br/>
+    <div >
+    <Card sx={{ maxWidth: 310, alignItems:'right'}}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Edit</Button>
+        <Button size="small">Delete</Button>
+      </CardActions>
+    </Card>
+    </div>
+    <div >
+    <Card sx={{ maxWidth: 310, }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Edit</Button>
+        <Button size="small">Delete</Button>
+      </CardActions>
+    </Card>
+    </div>
+
+    <div >
+    <Card sx={{ maxWidth: 310, alignItems:'right'}}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Edit</Button>
+        <Button size="small">Delete</Button>
+      </CardActions>
+    </Card>
+    </div>
+    </div>
     </>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,18 +1,39 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 import "../Components/NavBar.css"
 
 const NavBar = () => {
   return (
     <>
-    <div className="navbar">
-    <ul>
-             <li><NavLink acti to="/" className="nav-item">Idea</NavLink></li>
-             <li><NavLink to="/mentors" className="nav-item">Mentors</NavLink></li>
-             <li><NavLink to="/services" className="nav-item">Services</NavLink></li>
-             <li><NavLink to="/faqs" className="nav-item">FAQs</NavLink></li>
-         </ul>
+ <nav className="navbar navbar-expand-lg navbar-dark bg-dark ms-auto class within ">
+  {/* <a className="navbar-brand" href="#">Writify</a> */}
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse d-flex" id="navbarNavAltMarkup">
+    <div className="navbar-nav ">
+      <a className="nav-item nav-link active navbar-left" href="#">Idea <span className="sr-only"></span></a>
+      <a className="nav-item nav-link active" href="#">Mentors</a>
+      <a className="nav-item nav-link active" href="#">Services</a>
+      <a className="nav-item nav-link active" href="#">FAQs</a>
     </div>
+  </div>
+</nav>
+
+
+
+
+
+
+{/* 
+    <div classNameName="navbar">
+    <ul>
+             <li><NavLink acti to="/" classNameName="nav-item">Idea</NavLink></li>
+             <li><NavLink to="/mentors" classNameName="nav-item">Mentors</NavLink></li>
+             <li><NavLink to="/services" classNameName="nav-item">Services</NavLink></li>
+             <li><NavLink to="/faqs" classNameName="nav-item">FAQs</NavLink></li>
+         </ul>
+    </div> */}
     </>
   )
 }
