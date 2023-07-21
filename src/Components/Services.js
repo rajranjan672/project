92e3901aa5e36a2765064aa35049aae59553d500
Ujala singh <100126092+ujalasingh66@users.noreply.github.com>
2023-07-21 23:37:56 +0530

    dekho

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
-const Services = () => {
-  return (
-    <div>Services</div>
-  )
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
 
-export default Services
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+
+
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function Services() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        Item One
+        <h1> hey how r uu</h1>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Item Two
+        <h2> hey how r uu</h2>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+      <h3> hey how r uu</h3>
+        Item Three
+      </CustomTabPanel>
+    </Box>
+  );
+}
+
+
