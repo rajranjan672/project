e86378549994e74df8511223d764bab5d5f1ca46
raj.ranjan <raj.ranjan@greatmanagerinstitute.com>
2023-07-22 15:11:03 +0530

    tabs
    tabs changes

---
+++ src/Components/FoodRecipies.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FoodRecipies = () => {
+  return (
+    <div>FoodRecipies</div>
+  )
+}
+
+export default FoodRecipies
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,16 +1,17 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import MovieIcon from '@mui/icons-material/Movie';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import Movies from './Movies';
+import FoodRecipies from './FoodRecipies';
+import Travel from './Travel';
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function CustomTabPanel(props: TabPanelProps) {
+function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
 
   return (
@@ -24,51 +25,50 @@ function CustomTabPanel(props: TabPanelProps) {
       {value === index && (
         <Box sx={{ p: 3 }}>
           <Typography>{children}</Typography>
-
-
         </Box>
       )}
     </div>
   );
 }
 
-function a11yProps(index: number) {
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`,
   };
 }
 
-export default function Services() {
+export default function BasicTabs() {
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+        <Tabs variant="fullWidth"  color="secondary" value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab icon={<MovieIcon />} {...a11yProps(0)} />
+          <Tab icon={<FastfoodIcon />} {...a11yProps(1)} />
+          <Tab icon={<BeachAccessIcon />} {...a11yProps(2)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        Item One
-        <h1> hey check now</h1>
+        <Movies />
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
-        Item Two
-        <h2> hey how r uu</h2>
+        <FoodRecipies />
       </CustomTabPanel>
       <CustomTabPanel value={value} index={2}>
-      <h3> hey how r uu</h3>
-        Item Three
+        <Travel />
       </CustomTabPanel>
     </Box>
   );
 }
-
-
