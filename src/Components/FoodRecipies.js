e9f76f1a632af0ef48d9baa98013679434b6a1fe
raj.ranjan <raj.ranjan@greatmanagerinstitute.com>
2023-08-05 15:20:09 +0530

    responsive recipies
    res

--- src/Components/FoodRecipies.js
+++ src/Components/FoodRecipies.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./FoodRecipies.css";
+import { Box, Paper } from '@mui/material';
 
 const FoodRecipies = () => {
   return (
     <>
         <h2  className="centr">FoodRecipies</h2>
+        <div className='container'>
+          <div className='row'>
+3        <Paper className='paper col-10 col-sm-5 col-md-3 col-lg-5 my-3 py-1 mx-3' elevation={20} />
+          <Paper className='paper col-10 col-sm-5 col-md-3 col-lg-5 my-3 py-1 mx-3' elevation={20} />
+          <Paper className='paper col-10 col-sm-5 col-md-3 col-lg-5 my-3 py-1 mx-3' elevation={20} />
+          <Paper className='paper col-10 col-sm-5 col-md-3 col-lg-5 my-3 py-1 mx-3' elevation={20} />
+          </div>
+        </div>
+     
+
         </>
   )
 }
